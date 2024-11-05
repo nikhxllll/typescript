28d438a053a99749f0f187037c6c8c0ee814fb40
Nikhil Yadav <ny619619@gmail.com>
2024-11-05 16:29:17 +0530

    generic interface learned

---
+++ generic_interface.ts
@@ -0,0 +1,13 @@
+
+
+interface halua<T>{
+    name:string;
+    pudi:boolean;
+    key:T
+}
+
+function halua_pudi(obj:halua<string>){
+    return halua_pudi;
+}
+
+halua_pudi({name:"Gajar ka",pudi:true,key:"HALUAPUDIBEST"})
