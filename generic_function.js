a4a626ff22fb155951590ae60418b4b2aaf3844f
Nikhil Yadav <ny619619@gmail.com>
2024-11-05 16:20:31 +0530

    Generic funsction learned

---
+++ generic_function.js
@@ -0,0 +1,6 @@
+"use strict";
+function fun(a) {
+    console.log(a);
+}
+fun("Nikhil"); //fun("Nikhil") YOU CAN ALSO WRITE LIKE THIS DIRECTLY
+fun(12); //fun(12) YOU CAN ALSO WRITE LIKE THIS DIRECTLY
