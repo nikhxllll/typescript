7dc9c5211b93ef4f835e3cc58514799f70bf79af
Nikhil Yadav <ny619619@gmail.com>
2024-11-05 16:20:12 +0530

    Generic function learned

---
+++ generic_function.ts
@@ -0,0 +1,7 @@
+function fun<T>(a:T){
+    console.log(a)
+}
+
+fun<string>("Nikhil") //fun("Nikhil") YOU CAN ALSO WRITE LIKE THIS DIRECTLY
+
+fun<number>(12) //fun(12) YOU CAN ALSO WRITE LIKE THIS DIRECTLY
