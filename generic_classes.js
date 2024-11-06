d989656a149bd73648cdddf103c155e7ff217d76
Nikhil Yadav <ny619619@gmail.com>
2024-11-06 16:12:47 +0530

    Generic classes done

---
+++ generic_classes.js
@@ -0,0 +1,8 @@
+var BottleMaker = /** @class */ (function () {
+    function BottleMaker(name, key) {
+        this.name = name;
+    }
+    return BottleMaker;
+}());
+var b2 = new BottleMaker("MIlton", "");
+console.log(b2);
